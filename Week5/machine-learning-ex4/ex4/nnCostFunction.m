283a94b6f459aeaa38665db636ab456311efaa2c
Mohamed Shakeel <mshakeelibrahim@gmail.com>
2020-04-30 02:14:53 +0530

    Week5: Complete programming assignment

--- Week5/machine-learning-ex4/ex4/nnCostFunction.m
+++ Week5/machine-learning-ex4/ex4/nnCostFunction.m
@@ -66,17 +66,57 @@ Theta2_grad = zeros(size(Theta2));
 
 
 
+biased_input = [ones(m,1) X];
+a1 = biased_input;
+z2 = a1 * Theta1';
+a2 = [ones(size(z2),1) sigmoid(z2)];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+
+recoded_output_labels = zeros(num_labels, m);
+
+for i=1:m
+    recoded_output_labels(y(i), i) = 1;
+end
+
+J = -1 / m * sum((sum(recoded_output_labels .* log(a3') + (1 - recoded_output_labels) .* log(1 - a3'))));
+
+t1 = Theta1(:,2:size(Theta1,2));
+t2 = Theta2(:,2:size(Theta2,2));
+
+% Regularization
+Reg = lambda  * (sum( sum ( t1.^ 2 )) + sum( sum ( t2.^ 2 ))) / (2*m);
+
+% Regularized cost function
+J = J + Reg;
 
 
+for i=1:m
+    a1 = biased_input(i,:)';
+    z2 = Theta1 * a1;
+    a2 = sigmoid(z2);
+
+    a2 = [1; a2];
+    z3 = Theta2 * a2;
+    a3 = sigmoid(z3);
+
+    delta_3 = a3 - recoded_output_labels(:,i);
+
+    z2 = [1; z2];
+
+    delta_2 = (Theta2' * delta_3) .* sigmoidGradient(z2);
+    delta_2 = delta_2(2:end);
+
+    Theta2_grad = Theta2_grad + delta_3 * a2';
+    Theta1_grad = Theta1_grad + delta_2 * a1';
+end
+
+Theta2_grad = (1 / m) * Theta2_grad;
+Theta1_grad = (1 / m) * Theta1_grad;
 
 
-
-
-
-
-
-
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + ((lambda/m) * Theta1(:, 2:end));
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + ((lambda/m) * Theta2(:, 2:end));
 
 
 
